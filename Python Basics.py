0467e9120f87fe9d76209cb95ddeba15f16135b8
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2023-12-01 09:22:20 -0700

    Create Python Basics.py

---
+++ Python Basics.py
@@ -0,0 +1,20 @@
+# Tricky Counter Excersise
+
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+counter = 0
+
+for item in my_list:
+    
+    counter = counter + item
+
+print(counter) #this indentation will print that you get the totall of all the numbers 
+
+
+for i in my_list:
+    
+    counter2 = counter2 + item
+    print(counter2) # this indentation will give you each item in your list printed 
+
+
+#Range used for looping most commonly.
